d6df515c7308e2730fe996fa567488db6b658549
Albert-Fatkhudinov <fasberg2@gmail.com>
2020-05-03 15:27:04 +0300

    Palindrome

--- algoritm/src/main/java/palindrom/Palindrome.java
+++ algoritm/src/main/java/palindrom/Palindrome.java
@@ -3,8 +3,8 @@ package palindrom;
 public class Palindrome {
 
     public void checkPalindrome(String word) {
-        word = word.toLowerCase();
-        String palindromeWord = word.toLowerCase().replaceAll(" ", "");
+        word = word.toLowerCase().replaceAll("[^A-Za-z0-9]", "");
+        String palindromeWord = word.toLowerCase().replaceAll("[^A-Za-z0-9]", "");
         StringBuilder builder = new StringBuilder(palindromeWord).reverse();
         if (builder.toString().equals(word)) {
             System.out.println("yes");
